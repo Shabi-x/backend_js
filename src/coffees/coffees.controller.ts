de932416b65f9fc51fe755f5ebfa37a7881b52c8
shabi-x <2936670268@qq.com>
2024-03-25 21:05:37 +0800

    feat:'读取前端请求的body参数'

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -8,7 +8,16 @@ export class CoffeesController {
   }
   @Get('/:id')
   // findOne(@Param() params) {
+  //获取id参数
   findOne(@Param('id') id: string) {
     return `This action returns #${id} coffees`;
+  }
+
+  @Post()
+  // create(@Body('name') body) {
+  //只获取body中的name参数
+  create(@Body() body) {
+    //获取body中的所有参数
+    return body;
   }
 }
