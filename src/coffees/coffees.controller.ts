ab835a5450726f8becb05d448c70e95dfc350495
shabi-x <2936670268@qq.com>
2024-03-25 21:39:44 +0800

    feat:'调用@Res和@HttpCode，返回数据或者修改状态code'

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,10 +1,20 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll() {
-    return 'This action returns all coffees';
+  findAll(@Res() response) {
+    // return 'This action returns all coffees';
+    response.status(HttpStatus.OK).send('This action returns all coffees'); //使用response对象返回数据
   }
   @Get('/:id')
   // findOne(@Param() params) {
@@ -16,6 +26,7 @@ export class CoffeesController {
   @Post()
   // create(@Body('name') body) {
   //只获取body中的name参数
+  @HttpCode(HttpStatus.CREATED) //设置返回状态码为201 Created
   create(@Body() body) {
     //获取body中的所有参数
     return body;
