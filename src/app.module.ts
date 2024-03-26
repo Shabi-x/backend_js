00b79a03dfbbfaad0d74621ea8192092afc9453a
shabi-x <2936670268@qq.com>
2024-03-26 21:10:27 +0800

    feat:"启动docker，引入typerom，连接postgres数据库"

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 // import { CoffeesController } from './coffees/coffees.controller';
 // import { CoffeesService } from './coffees/coffees.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule], //使用命令nest g module 生成
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ], //使用命令nest g module 生成
   controllers: [AppController], //使用命令nest g controller 生成
   providers: [AppService], //使用命令nest g service 生成
 })
