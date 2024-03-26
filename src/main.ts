7e20d171bb008eac71ed6255c180a03857dbf819
shabi-x <2936670268@qq.com>
2024-03-26 20:01:14 +0800

    feat:"ValidationPipe的transform类型转换？没懂是干什么的"

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true, //ValidationPipe的白名单，配合dto使用，可以限制请求参数的字段
       forbidNonWhitelisted: true, //禁止传入非白名单字段,会直接报错，超出白名单的字段会被提示不应该存在
+      transform: true, //将请求参数转换成DTO类型
     }),
   );
   await app.listen(3001);
